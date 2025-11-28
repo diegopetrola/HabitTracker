9031f6828014bad37db8153d46e37a64bd399af4
Diego Leal <diegopetrola@gmail.com>
2025-11-28 03:01:21 -0300

    Initial commit

---
+++ HabitTrackerProgram/Migrations/20251128053209_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HabitTrackerProgram.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Habit",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habit", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Habit");
+        }
+    }
+}
---
+++ HabitTrackerProgram/Models/Habit.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace HabitTrackerProgram.Models;
+
+public class HabitContext : DbContext
+{
+    public DbSet<Habit> Habit{ get; set; }
+    public string DbPath { get; }
+
+    public HabitContext()
+    {
+        var folder =  Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Habit
+{
+    public int Id { get; set; }
+    public string Description { get; set; } = "";
+    public DateOnly Date { get; set; }
+    public int Quantity { get; set; }
+}
