5c1f40ad82836b762211284620c0e3f7b855253b
Diego Leal <diegopetrola@gmail.com>
2025-12-01 10:08:37 -0300

    removed some comments

--- HabitTrackerProgram/Program.cs
+++ HabitTrackerProgram/Program.cs
@@ -1,19 +1,4 @@
-﻿//This habit can't be tracked by time (ex. hours of sleep), only by quantity (ex. number of water glasses a day)
-//Users need to be able to input the date of the occurrence of the habit
-//The application should store and retrieve data from a real database
-//When the application starts, it should create a sqlite database, if one isn’t present.
-//It should also create a table in the database, where the habit will be logged.
-//The users should be able to insert, delete, update and view their logged habit.
-//You should handle all possible errors so that the application never crashes.
-//You can only interact with the database using ADO.NET.You can’t use mappers such as Entity Framework or Dapper.
-//Your project needs to contain a Read Me file where you'll explain how your app works.
-
-// Minor concerns:
-// Code was done using EntityFrameworkCore
-// the "Screen" functions are dealing with business logic
-
-
-using HabitTrackerProgram.Models;
+﻿using HabitTrackerProgram.Models;
 using Microsoft.EntityFrameworkCore;
 using var db = new HabitContext();
 
